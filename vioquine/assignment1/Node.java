46589537c2c91e748258c4ac20bae9cd36ef9318
Anne Wissing <awi2308@gmail.com>
2018-05-19 00:09:11 +0200

    Added Node class to describe one element of a SingleLinkedList

---
+++ vioquine/assignment1/Node.java
@@ -0,0 +1,25 @@
+public class Node<T> {
+    private T content;
+    private Node next;
+
+    public Node(T content, Node<T> next) {
+        this.content = content;
+        this.next = next;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
