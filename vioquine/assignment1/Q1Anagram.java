0d07cf446d6ec90a8f3b76c24cff4acf31c82ad5
Anne Wissing <awi2308@gmail.com>
2018-05-20 11:22:33 +0200

    Updated comments in Q1Anagram.java

--- vioquine/assignment1/Q1Anagram.java
+++ vioquine/assignment1/Q1Anagram.java
@@ -2,26 +2,31 @@ import java.util.HashMap;
 
 public class Q1Anagram {
     public static void main(String... args){
-
-        System.out.println(checkAnagram("",""));
-        System.out.println(checkAnagram("Fourth of July","Joyful Fourth"));
-        System.out.println(checkAnagram("Fourth of July!","Joyful Fourth"));
-        System.out.println(checkAnagram("Fourth of July","Joyful Fourth?"));
-        System.out.println(checkAnagram("Forth of July","Joyful Fourth"));
-        System.out.println(checkAnagram("Forth of July","Joyful fourth"));
+        System.out.println(checkAnagram("","",true));
+        System.out.println(checkAnagram("Fourth of July","Joyful Fourth",true));
+        System.out.println(checkAnagram("Fourth of July!","Joyful Fourth",true));
+        System.out.println(checkAnagram("Fourth of July","Joyful Fourth?",true));
+        System.out.println(checkAnagram("Forth of July","Joyful Fourth",true));
+        System.out.println(checkAnagram("Forth of July","Joyful fourth",true));
+        System.out.println(checkAnagram("Fourth of July","joyful fourth",false));
+        System.out.println(checkAnagram("Fourth of July","joyful forth",false));
     }
 
     /**
      * Checks if two given sentences are anagrams (case sensitive)
      * @param sentence1
      * @param sentence2
+     * @param caseSensitive true =c ase sensitive check; false = case insensitive check
      * @return true, if they are anagrams; false, otherwise
      */
-    private static boolean checkAnagram(String sentence1, String sentence2){
-        // Normalize the sentences (Remove whitespaces and punctuation)
+    private static boolean checkAnagram(String sentence1, String sentence2, boolean caseSensitive){
         sentence1 = normalizeString(sentence1);
         sentence2 = normalizeString(sentence2);
 
+        if (!caseSensitive){
+            sentence1 = sentence1.toLowerCase();
+            sentence2 = sentence2.toLowerCase();
+        }
         // If sentence1 is longer/shorter than sentence2, they can't be anagrams
         if (sentence1.length()!= sentence2.length()){
             return false;
@@ -30,10 +35,8 @@ public class Q1Anagram {
         // Create a map to store all characters in sentence1 with their occurrences
         HashMap<Character,Integer> characters = new HashMap<>();
 
-        // Go through sentence1
         for(int i=0; i<sentence1.length(); i++){
             char c = sentence1.charAt(i);
-            // Check if the current character c is already in the map
             if (characters.containsKey(c)){
                 // The character is in the map, so just increment the occurrence
                 characters.replace(c,characters.get(c) + 1);
@@ -43,7 +46,6 @@ public class Q1Anagram {
             }
         }
 
-        // Go through sentence2
         for(int i=0; i<sentence2.length(); i++){
             char c = sentence2.charAt(i);
             // If c is not in the characters map, sentence2 can't be an anagram of sentence1
@@ -52,7 +54,6 @@ public class Q1Anagram {
             }
             int occurrence = characters.get(c);
             if(occurrence > 1){
-                // Decrement the occurrence of c
                 characters.replace(c,occurrence-1);
             }else {
                 // Remove c from the map, because all occurrences are used
