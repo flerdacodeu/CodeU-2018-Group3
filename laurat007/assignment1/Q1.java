48161fec6a1e52a05e0a1eb98f47b4e604076642
Laura Cucu <lauratdn_7984@yahoo.ro>
2018-06-04 00:36:07 +0300

    Update Q1.java

--- laurat007/assignment1/Q1.java
+++ laurat007/assignment1/Q1.java
@@ -1,3 +1,4 @@
+package assignment_1;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -32,102 +33,114 @@ public class Q1 {
 	
 	// number of pairs to be checked for being anagrams
 	static int N;
+        static Scanner sc;
 	
 	static boolean caseSensitiveAnagrams(String s1, String s2) {
-		System.out.println(s1 + " " + s2);
-		if(s1.length() != s2.length())
-			return false;
-		
-		// 58 = 122('z') - 65('A') + 1
-		int letters_occurrences[] = new int[58];
-		for(int i = 0 ; i < 58; i++)
-			letters_occurrences[i] = 0;
-		for(int i = 0; i < s1.length(); i++) {
-			letters_occurrences[122 - (int)s1.charAt(i)]++;
-		}
-		for(int i = 0; i < s1.length(); i++) {
-			letters_occurrences[122 - (int)s2.charAt(i)]--;
-		}
-		for(int i = 0 ; i < 57; i++)
-			if(letters_occurrences[i] != 0)
-				return false;
-		return true;
+            System.out.println(s1 + " " + s2);
+            if(s1.length() != s2.length())
+                return false;
+
+            // 58 = 122('z') - 65('A') + 1
+            int letters_occurrences[] = new int[58];
+            for(int i = 0 ; i < 58; i++)
+                letters_occurrences[i] = 0;
+            for(int i = 0; i < s1.length(); i++) {
+                letters_occurrences[122 - (int)s1.charAt(i)]++;
+            }
+            for(int i = 0; i < s1.length(); i++) {
+                letters_occurrences[122 - (int)s2.charAt(i)]--;
+            }
+            for(int i = 0 ; i < 57; i++)
+                if(letters_occurrences[i] != 0)
+                    return false;
+            return true;
 	}
+        
+        static boolean caseInsensitiveAnagrams(String s1, String s2) {
+            return caseSensitiveAnagrams(s1.toLowerCase(), s2.toLowerCase());
+        }
+        
+        static boolean caseInsensitiveSentenceAnagrams(ArrayList<String> s1, ArrayList<String> s2) {
+            if(s1.size() != s2.size()) {
+                return false;
+            }
+
+            // check if each word in first sentence
+            // matches any word in the second sentence
+            int exit = 0;
+            for(int j = 0; j < s1.size(); j++) {
+                exit = 0;
+                for(int k = 0; k < s2.size(); k++) {
+                    if(caseSensitiveAnagrams(s1.get(j).toLowerCase(),
+                                    s2.get(k).toLowerCase())) {
+                        exit = 1;
+                        s2.remove(k);
+                        break;
+                    }
+                }
+                if(exit == 0) {
+                    return false;
+                }
+            }
+            if(s2.isEmpty()) 
+                return true;
+            return false;
+        }
 	
-	static getResult() {
-		try {
-			Scanner sc = new Scanner(new BufferedReader(new FileReader(
-				INPUT_FILE)));
-			N = Integer.parseInt(sc.nextLine());
-			for(int i = 0; i < N; i++) {
-				String buffer = sc.nextLine();
-				StringTokenizer token = new StringTokenizer(buffer, " .,");
-				int identifier = Integer.parseInt(token.nextToken());
-				
-				// case sensitive
-				if(identifier == 0) {
-					System.out.println(caseSensitiveAnagrams(token.nextToken(), token.nextToken()));
-				}
-					
-				else {
-					
-					// case insensitive
-					if(identifier == 1) {
-						System.out.println(caseSensitiveAnagrams(token.nextToken().toLowerCase(), 
-							token.nextToken().toLowerCase()));
-					}
-					
-					// phrase + case insensitive
-					else {
-						ArrayList<String> first_sentence = new ArrayList<>();
-						while(token.hasMoreTokens()) {
-							first_sentence.add(token.nextToken());
-						}
-						System.out.println(first_sentence);
-						buffer = sc.nextLine();
-						StringTokenizer token2 = new StringTokenizer(buffer, " .,");
-						
-						// second sentence is on the next line
-						ArrayList<String> second_sentence = new ArrayList<>();
-						while(token2.hasMoreTokens()) {
-							second_sentence.add(token2.nextToken());
-						}
-						System.out.println(second_sentence);
-						if(first_sentence.size() != second_sentence.size()) {
-							System.out.println(false);
-							continue;
-						}
-						
-						// check if each word in first sentence
-						// matches any word in the second sentence
-						int exit = 0;
-						for(int j = 0; j < first_sentence.size(); j++) {
-							exit = 0;
-							for(int k = 0; k < second_sentence.size(); k++) {
-								if(caseSensitiveAnagrams(first_sentence.get(j).toLowerCase(),
-										second_sentence.get(k).toLowerCase())) {
-									exit = 1;
-									second_sentence.remove(k);
-									break;
-								}
-							}
-							if(exit == 0) {
-								System.out.println(false);
-								break;
-							}
-						}
-						if(exit == 1) 
-							System.out.println(true);
-					}
-				}
-			}
-			sc.close();
-		} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
+	static void getResult() {
+            try {
+                sc = new Scanner(new BufferedReader(new FileReader(
+                        INPUT_FILE)));
+                N = Integer.parseInt(sc.nextLine());
+                for(int i = 0; i < N; i++) {
+                    String buffer = sc.nextLine();
+                    StringTokenizer token = new StringTokenizer(buffer, " .,");
+                    int identifier = Integer.parseInt(token.nextToken());
+
+                    // case sensitive
+                    if(identifier == 0) {
+                        System.out.println(caseSensitiveAnagrams(token.nextToken(), token.nextToken()));
+                    }
+
+                    else {
+
+                        // case insensitive
+                        if(identifier == 1) {
+                            System.out.println(caseInsensitiveAnagrams(token.nextToken(), token.nextToken()));
+                        }
+
+                        // phrase + case insensitive
+                        else {
+                            
+                            // read first sentence
+                            ArrayList<String> first_sentence = new ArrayList<>();
+                            while(token.hasMoreTokens()) {
+                                    first_sentence.add(token.nextToken());
+                            }
+                            System.out.println(first_sentence);
+                            buffer = sc.nextLine();
+                            StringTokenizer token2 = new StringTokenizer(buffer, " .,");
+
+                            // second sentence is on the next line
+                            ArrayList<String> second_sentence = new ArrayList<>();
+                            while(token2.hasMoreTokens()) {
+                                    second_sentence.add(token2.nextToken());
+                            }
+                            System.out.println(second_sentence);
+                            
+                            // apply test
+                            System.out.println(caseInsensitiveSentenceAnagrams(first_sentence, second_sentence));
+                        }
+                    }
+                }
+                sc.close();
+            } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
 	}
 	
 	public static void main(String[] args) {
 		getResult();
 	}
 }
+
