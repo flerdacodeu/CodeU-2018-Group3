d44c8c87f32f75d7d80a0d6ab9a7631f03ef68e4
camligorkem <camligorkem@gmail.com>
2018-06-03 18:20:11 +0200

    Create BinaryNode.java

---
+++ camligorkem/assignment2/BinaryNode.java
@@ -0,0 +1,35 @@
+public class BinaryNode<T> {
+    private T data;
+    private BinaryNode<T> left;
+    private BinaryNode<T> right;
+
+    public BinaryNode(T data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public BinaryNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(BinaryNode<T> left) {
+        this.left = left;
+    }
+
+    public BinaryNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(BinaryNode<T> right) {
+        this.right = right;
+    }
+}
