514bdd712ea17281bde9f5a75269295323a2e57d
Anne Wissing <awi2308@gmail.com>
2018-06-18 18:30:48 +0200

    Added bigger test case for word search

--- vioquine/assignment3/WordSearchTest.java
+++ vioquine/assignment3/WordSearchTest.java
@@ -7,20 +7,13 @@ import static org.junit.Assert.assertEquals;
 
 public class WordSearchTest {
 
-    Dictionary dictionary;
-    char[][] grid;
-
-    @Before
-    public void prepare(){
-        dictionary = new Dictionary(Arrays.asList("CAR", "CARD", "CART", "CAT"));
-        grid = new char[][]{{'a', 'a', 'r'}, {'t', 'c', 'd'}};
-    }
-
     /**
      * Test for the example on the assignment
      */
     @Test
     public void testFromAssignment() {
+        Dictionary dictionary = new Dictionary(Arrays.asList("CAR", "CARD", "CART", "CAT"));
+        char[][] grid = new char[][]{{'a', 'a', 'r'}, {'t', 'c', 'd'}};
         Set<String> expectedResult = new HashSet<>(Arrays.asList("car", "card", "cat"));
 
         Set<String> actualResult = WordSearch.searchWords(dictionary, grid);
@@ -29,7 +22,22 @@ public class WordSearchTest {
     }
 
     @Test
+    public void testBiggerGid() {
+        Dictionary dictionary = new Dictionary(Arrays.asList("CAR", "CARD", "CART", "CAT", "DEEP", "EAT", "BIT", "BUILD"));
+        char[][] grid = new char[][]{{'a', 'a', 'r', 'b'}, {'t', 'c', 'd', 'i'}, {'p', 'e', 'e', 't'}, {'a', 't', 'b', 'y'}};
+        Set<String> expectedResult = new HashSet<>(Arrays.asList("car", "card", "cat","deep","eat","bit"));
+
+        Set<String> actualResult = WordSearch.searchWords(dictionary, grid);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    //TODO more tests
+
+    @Test
     public void testCheckForPosition(){
+        Dictionary dictionary = new Dictionary(Arrays.asList("CAR", "CARD", "CART", "CAT"));
+        char[][] grid = new char[][]{{'a', 'a', 'r'}, {'t', 'c', 'd'}};
         boolean[][] visited = new boolean[][]{{true,false,false},{false,true,false}};
         assertEquals(WordSearch.checkForPosition(dictionary,grid,0,0,"ca",visited),Optional.empty());
         assertEquals(WordSearch.checkForPosition(dictionary,grid,1,0,"ca",visited),Optional.of(new HashSet<>(Collections.singletonList("cat"))));
