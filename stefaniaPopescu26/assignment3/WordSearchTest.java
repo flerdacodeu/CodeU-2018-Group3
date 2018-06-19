9ec564af60e3bc4a38f80e0c2e1f6be7cb1e96fb
Stefania Popescu <38551766+stefaniaPopescu26@users.noreply.github.com>
2018-06-19 13:28:53 +0300

    Create WordSearchTest.java

---
+++ stefaniaPopescu26/assignment3/WordSearchTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class WordSearchTest {
+
+    @Test
+    public void getWords_emptyGrid() {
+        WordSearch ws = new WordSearch("wordSearch.txt", "dictionary.txt");
+        ws.setGrid(0,0, new String[0][0]);
+        assertEquals(null, ws.getWords());
+    }
+
+    @Test
+    public void getWords_emptyDictionary() {
+        WordSearch ws = new WordSearch("wordSearch.txt", "dictionary.txt");
+        ws.delDictionary();
+        assertEquals(new ArrayList(), ws.getWords());
+    }
+
+    @Test
+    public void getWords_validInput() {
+        WordSearch ws = new WordSearch("wordSearch.txt", "dictionary.txt");
+        List<String> list = new ArrayList<>();
+        list.add("CAT");
+        list.add("CAR");
+        list.add("CARD");
+        assertEquals(list, ws.getWords());
+    }
+}
