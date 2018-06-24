61351dafc860a268db1c98c2eff126173d2850e1
Anne Wissing <awi2308@gmail.com>
2018-06-24 11:35:40 +0200

    Removed duplicated code and checkForPosition method

--- vioquine/assignment3/WordSearch.java
+++ vioquine/assignment3/WordSearch.java
@@ -1,5 +1,4 @@
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 class WordSearch {
@@ -45,61 +44,15 @@ class WordSearch {
         if (dictionary.isWord(prefix)) {
             foundWords.add(prefix);
         }
-        //up right
-        if (row - 1 >= 0 && column + 1 < grid[row].length) {
-            checkForPosition(dictionary, grid, row - 1, column + 1, prefix, visited).ifPresent(foundWords::addAll);
-        }
-        //right
-        if (column + 1 < grid[row].length) {
-            checkForPosition(dictionary, grid, row, column + 1, prefix, visited).ifPresent(foundWords::addAll);
-        }
-        //down right
-        if (row + 1 < grid.length && column + 1 < grid[row].length) {
-            checkForPosition(dictionary, grid, row + 1, column + 1, prefix, visited).ifPresent(foundWords::addAll);
-        }
-        // down
-        if (row + 1 < grid.length) {
-            checkForPosition(dictionary, grid, row + 1, column, prefix, visited).ifPresent(foundWords::addAll);
-        }
-        //up left
-        if (row - 1 >= 0 && column - 1 >= 0) {
-            checkForPosition(dictionary, grid, row - 1, column - 1, prefix, visited).ifPresent(foundWords::addAll);
-        }
-        //left
-        if (column - 1 >= 0) {
-            checkForPosition(dictionary, grid, row, column - 1, prefix, visited).ifPresent(foundWords::addAll);
-        }
-        //down left
-        if (row + 1 < grid.length && column - 1 >= 0) {
-            checkForPosition(dictionary, grid, row + 1, column - 1, prefix, visited).ifPresent(foundWords::addAll);
-        }
-        //up
-        if (row - 1 >= 0) {
-            checkForPosition(dictionary, grid, row - 1, column, prefix, visited).ifPresent(foundWords::addAll);
+        for (int i = Math.max(0, row - 1); i <= Math.min(row + 1, grid.length - 1); i++) {
+            for (int j = Math.max(0, column - 1); j <= Math.min(column + 1, grid[row].length - 1); j++) {
+                if (!visited[i][j] && dictionary.isPrefix(prefix + grid[i][j])) {
+                    visited[i][j] = true;
+                    foundWords.addAll(searchWordsFromPosition(dictionary, grid, i, j, prefix + grid[i][j], visited));
+                    visited[i][j] = false;
+                }
+            }
         }
         return foundWords;
-    }
-
-    /**
-     * Checks, if the position can be used to form a word.
-     * If it can be used, the search for the words that can be formed will be run
-     * It it can't be used (because it was already visited or it is not part of an available prefix), an empty Optional will be returned
-     *
-     * @param dictionary dictionary of the words
-     * @param grid       grid with characters in lower case
-     * @param row        current row
-     * @param column     current column
-     * @param prefix     current formed prefix
-     * @param visited    information about visited positions
-     * @return if the given position wasn't visited and prefix with the value of the position is also a prefix, it returns the set of found words. Otherwise it return an empty optional
-     */
-    static Optional<Set<String>> checkForPosition(Dictionary dictionary, char[][] grid, int row, int column, String prefix, boolean[][] visited) {
-        if (!visited[row][column] && dictionary.isPrefix(prefix + grid[row][column])) {
-            visited[row][column] = true;
-            Set<String> result = searchWordsFromPosition(dictionary, grid, row, column, prefix + grid[row][column], visited);
-            visited[row][column] = false;
-            return Optional.of(result);
-        }
-        return Optional.empty();
     }
 }
