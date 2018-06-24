ad86547b3747a6264986b83fd3f2337d93518e11
Anne Wissing <awi2308@gmail.com>
2018-06-24 11:21:16 +0200

    Changed imports from .* to specific packages

--- vioquine/assignment3/WordSearchTest.java
+++ vioquine/assignment3/WordSearchTest.java
@@ -1,7 +1,9 @@
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,7 +27,7 @@ public class WordSearchTest {
     public void testBiggerGid() {
         Dictionary dictionary = new Dictionary(Arrays.asList("CAR", "CARD", "CART", "CAT", "DEEP", "EAT", "BIT", "BUILD"));
         char[][] grid = new char[][]{{'a', 'a', 'r', 'b'}, {'t', 'c', 'd', 'i'}, {'p', 'e', 'e', 't'}, {'a', 't', 'b', 'y'}};
-        Set<String> expectedResult = new HashSet<>(Arrays.asList("car", "card", "cat","deep","eat","bit"));
+        Set<String> expectedResult = new HashSet<>(Arrays.asList("car", "card", "cat", "deep", "eat", "bit"));
 
         Set<String> actualResult = WordSearch.searchWords(dictionary, grid);
 
@@ -33,11 +35,11 @@ public class WordSearchTest {
     }
 
     @Test
-    public void testCheckForPosition(){
+    public void testCheckForPosition() {
         Dictionary dictionary = new Dictionary(Arrays.asList("CAR", "CARD", "CART", "CAT"));
         char[][] grid = new char[][]{{'a', 'a', 'r'}, {'t', 'c', 'd'}};
-        boolean[][] visited = new boolean[][]{{true,false,false},{false,true,false}};
-        assertEquals(WordSearch.checkForPosition(dictionary,grid,0,0,"ca",visited),Optional.empty());
-        assertEquals(WordSearch.checkForPosition(dictionary,grid,1,0,"ca",visited),Optional.of(new HashSet<>(Collections.singletonList("cat"))));
+        boolean[][] visited = new boolean[][]{{true, false, false}, {false, true, false}};
+        assertEquals(WordSearch.checkForPosition(dictionary, grid, 0, 0, "ca", visited), Optional.empty());
+        assertEquals(WordSearch.checkForPosition(dictionary, grid, 1, 0, "ca", visited), Optional.of(new HashSet<>(Arrays.asList("cat"))));
     }
 }
