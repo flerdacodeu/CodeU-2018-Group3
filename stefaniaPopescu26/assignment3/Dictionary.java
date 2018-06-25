9e2fb1ecece7010fdca83637bca77a001da0832e
Stefania Popescu <38551766+stefaniaPopescu26@users.noreply.github.com>
2018-06-26 02:31:01 +0300

    Update Dictionary.java

--- stefaniaPopescu26/assignment3/Dictionary.java
+++ stefaniaPopescu26/assignment3/Dictionary.java
@@ -2,21 +2,43 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Set;
 
 /*
-    I implemented the dictionary with a HashMap, so that it will
-    check if a word is in the dictionary with a O(1) complexity
-    and if a sequence is a prefix with a O(n) complexity.
+    For the Dictionary class I implemented the Trie data structure.
+    Notes:
+        n = number of words in the dictionary
+        m = maximum length of a word
+    Complexities:
+        add - O(m)
+        readData - O(n * m)
+        isPrefix - O(m)
+        isWord - O(m)
  */
 
-public class Dictionary extends HashMap<String, Object>{
+public class Dictionary {
+
+    class TrieNode {
+        HashMap<Character, TrieNode> children;
+        boolean isWord;
+
+        public TrieNode() {
+            children = new HashMap<>();
+            isWord = false;
+        }
+    }
+
+    private TrieNode root;
+
+    public Dictionary() {
+        root = new TrieNode();
+    }
 
     public Dictionary(String filename) {
+        root = new TrieNode();
         readData(filename);
     }
 
-    public void readData(String filename) {
+    private void readData(String filename) {
         try {
             Scanner sc = new Scanner(new File(filename));
 
@@ -25,7 +47,7 @@ public class Dictionary extends HashMap<String, Object>{
                 sc.nextLine();
 
             for(int i = 0; i < nrWords; i++) {
-                put(sc.next(), true);
+                add(sc.next());
                 if (i != nrWords - 1)
                     sc.nextLine();
             }
@@ -36,21 +58,70 @@ public class Dictionary extends HashMap<String, Object>{
         }
     }
 
-    public boolean isWord(String word) {
-        return containsKey(word);
+    private void add(String word) {
+        if (word.isEmpty()) {
+            return;
+        }
+
+        TrieNode node = root;
+
+         do {
+            char letter = word.charAt(0);
+
+            if (!node.children.containsKey(letter)) {
+                node.children.put(letter, new TrieNode());
+            }
+
+            node = node.children.get(letter);
+            word = word.substring(1);
+        } while (!word.isEmpty());
+
+         node.isWord = true;
     }
 
     public boolean isPrefix(String prefix) {
-        if (prefix == null)
+        if (prefix.isEmpty()) {
             return false;
-
-        Set<String> keySet = keySet();
-
-        for (String str : keySet) {
-            if (str.startsWith(prefix))
-                return true;
         }
 
-        return false;
+        TrieNode node = root;
+
+        do {
+            char letter = prefix.charAt(0);
+
+            if (!node.children.containsKey(letter)) {
+                return false;
+            }
+
+            node = node.children.get(letter);
+            prefix = prefix.substring(1);
+        } while (!prefix.isEmpty());
+
+        return true;
+    }
+
+    public boolean isWord(String word) {
+        if (word.isEmpty()) {
+            return false;
+        }
+
+        TrieNode node = root;
+
+        do {
+            char letter = word.charAt(0);
+
+            if (!node.children.containsKey(letter)) {
+                return false;
+            }
+
+            node = node.children.get(letter);
+            word = word.substring(1);
+        } while (!word.isEmpty());
+
+        return node.isWord;
+    }
+
+    public boolean isEmpty() {
+        return root.children.isEmpty();
     }
 }
