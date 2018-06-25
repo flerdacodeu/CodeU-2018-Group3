33ab5ebba00d23c58a71999ec3f3083c119e4046
camligorkem <camligorkem@gmail.com>
2018-06-25 16:14:38 +0200

    Update WordSearchTest.java

--- camligorkem/assignment3/WordSearchTest.java
+++ camligorkem/assignment3/WordSearchTest.java
@@ -2,14 +2,13 @@ import org.junit.Before;
 import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Optional;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class WordSearchTest {
     Dictionary dic;
     Dictionary dic2;
-    Optional<HashSet<String>> answer;
+    HashSet<String> answer;
     char[][] grid = {{'a', 'a', 'r'}, {'t', 'c', 'd'},{'t','r','e'}};
     char[][] grid2 = {{'a', 'a', 'r'}, {'t', 'c', 'd'},{'t','r','e'},{'e','e','e'}};
 
@@ -25,9 +24,7 @@ public class WordSearchTest {
         for(String word: words2){
             dic2.insert(word);
         }
-        HashSet<String> set = new HashSet<>();
-        set.addAll(Arrays.asList("car", "card", "cat"));
-        answer= Optional.of(set);
+        answer = new HashSet<>(Arrays.asList("car", "card", "cat"));
     }
 
     @Test
@@ -35,16 +32,16 @@ public class WordSearchTest {
 
         assertEquals(answer, WordSearch.findWords(dic, grid));
         //false word
-        Optional<HashSet<String>> ans2 = Optional.of(new HashSet<>(Arrays.asList("car", "card", "cat", "apple")));
+        HashSet<String> ans2 = new HashSet<>(Arrays.asList("car", "card", "cat", "apple"));
         assertNotEquals(ans2, WordSearch.findWords(dic, grid));
         //prefix word
-        Optional<HashSet<String>> ans3 = Optional.of(new HashSet<>(Arrays.asList("car", "card", "ca")));
+        HashSet<String> ans3 = new HashSet<>(Arrays.asList("car", "card", "ca"));
         assertNotEquals(ans3, WordSearch.findWords(dic, grid));
         //can't use same letter in same position in grid twice for creation of single word
-        Optional<HashSet<String>> ans4 = Optional.of(new HashSet<>(Arrays.asList("car", "card", "cat", "tree")));
+        HashSet<String>ans4 = new HashSet<>(Arrays.asList("car", "card", "cat", "tree"));
         assertEquals(ans4, WordSearch.findWords(dic2, grid2));
         //false word 2
-        Optional<HashSet<String>> ans5 = Optional.of(new HashSet<>(Arrays.asList("car", "card", "cat", "care")));
+        HashSet<String> ans5 = new HashSet<>(Arrays.asList("car", "card", "cat", "care"));
         assertNotEquals(ans5, WordSearch.findWords(dic2, grid2));
     }
 }
