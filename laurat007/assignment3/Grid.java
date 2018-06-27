6a5377066aaaab48e40b3e57b17fffe047781b55
Laura Cucu <lauratdn_7984@yahoo.ro>
2018-06-28 00:32:12 +0300

    Grid.java

---
+++ laurat007/assignment3/Grid.java
@@ -0,0 +1,41 @@
+
+import java.util.TreeSet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Laura
+ *  Defining a grid with fixed number of columns and rows.
+ */
+class Grid {
+    
+    Dictionary dictionary;
+    int rows, columns;
+    char[][] grid;
+    
+    Grid(int r, int c, char[][] letterMatrix, Dictionary dict) {
+        dictionary = dict;
+        rows = r;
+        columns = c;
+        grid = new char[rows][columns];
+        for(int i = 0; i < rows; i++)
+            for(int j = 0; j < columns; j++) {
+                grid[i][j] = letterMatrix[i][j];
+            }
+    }
+    
+    // check if the cell exists in grid and is not visited yet
+    boolean isValid(int x, int y, boolean[][] visited) {
+        if(x >= 0 && x < rows)
+            if(y >= 0 && y < columns)
+                if(visited[x][y] == false)
+                    return true;
+        return false;
+    }  
+}
+
