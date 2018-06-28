c69527805a1bebf312abf56d646bb674bfc53e92
Stefania Popescu <38551766+stefaniaPopescu26@users.noreply.github.com>
2018-06-28 20:51:43 +0300

    Update WordSearchTest.java
    Imported only what I needed.

--- stefaniaPopescu26/assignment3/WordSearchTest.java
+++ stefaniaPopescu26/assignment3/WordSearchTest.java
@@ -1,9 +1,6 @@
 import org.junit.Test;
-
 import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class WordSearchTest {
 
