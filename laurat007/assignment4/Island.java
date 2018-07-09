36dc74b02f74b9322cbdd11764177a8a9c5f38a6
Laura Cucu <lauratdn_7984@yahoo.ro>
2018-07-09 23:33:37 +0300

    Update Island.java
    Added method to unify 2 areas. I chose to still use pairMap and try to explain why it would useful. I am not really sure how to replace it.

--- laurat007/assignment4/Island.java
+++ laurat007/assignment4/Island.java
@@ -17,6 +17,9 @@ import java.util.HashMap;
  *  - each neighbor is then tested: if it is not mapped yet, it will be mapped
  * in the same area/ if it is mapped the original cell will be mapped in
  * neighbor's area
+ *  - when a true cell already mapped is found, I find the minimum area of 
+ * its neighbors and, if it is lower than the current area, I put the minimum
+ * value found to all neighbors, and to current cell.
  */
 
 
@@ -49,51 +52,28 @@ public class Island {
         }
         
         // the land was already processed
-        else return;
+        else {
+            unifyNeighbors(mapIslands, i, j, pairMap);
+            return;
+        }
 
         // check neighbours
-        // up
-        if(isLand(i - 1, j, mapIslands, rows, columns, map, countIslands, pairMap)){
-            // already mapped neighbor
-            if(mapIslands.containsKey(pairMap[i - 1][j])) {
-                
-                // the original cell gets mapped to neighbor's area
-                // extra area is deleted
-                mapIslands.put(pairMap[i][j], mapIslands.get(pairMap[i - 1][j]));
-                countIslands--;
-            }
-            else
-                mapIslands.put(pairMap[i - 1][j], mapIslands.get(pairMap[i][j]));
-        }
-
-        // left
-        if(isLand(i, j - 1, mapIslands, rows, columns, map, countIslands, pairMap)){
-            if(mapIslands.containsKey(pairMap[i][j - 1])) {
-                mapIslands.put(pairMap[i][j], mapIslands.get(pairMap[i][j - 1]));
-                countIslands--;
-            }
-            else
-                mapIslands.put(pairMap[i][j - 1], mapIslands.get(pairMap[i][j]));
-        }
-
-        // right
-        if(isLand(i, j + 1, mapIslands, rows, columns, map, countIslands, pairMap)){
-            if(mapIslands.containsKey(pairMap[i][j + 1])) {
-                mapIslands.put(pairMap[i][j], mapIslands.get(pairMap[i][j + 1]));
-                countIslands--;
-            }
-            else
-                mapIslands.put(pairMap[i][j + 1], mapIslands.get(pairMap[i][j]));
-        }
-
-        // down
-        if(isLand(i + 1, j, mapIslands, rows, columns, map, countIslands, pairMap)){
-            if(mapIslands.containsKey(pairMap[i + 1][j])) {
-                mapIslands.put(pairMap[i][j], mapIslands.get(pairMap[i + 1][j]));
-                countIslands--;
-            }
-            else
-                mapIslands.put(pairMap[i + 1][j], mapIslands.get(pairMap[i][j]));
+        int[] deltaX = {i - 1, i, i, i + 1};
+        int[] deltaY = {j, j - 1, j + 1, j};
+        
+        for(int k = 0; k < 4; k++) {
+            if(isLand(deltaX[k], deltaY[k], mapIslands, rows, columns, map, countIslands, pairMap)) {
+               
+                // already mapped neighbor
+                if(mapIslands.containsKey(pairMap[deltaX[k]][deltaY[k]])) {
+                    // the original cell gets mapped to neighbor's area
+                    // extra area is deleted
+                    mapIslands.put(pairMap[i][j], mapIslands.get(pairMap[deltaX[k]][deltaY[k]]));
+                    countIslands--;
+                }
+                else
+                    mapIslands.put(pairMap[deltaX[k]][deltaY[k]], mapIslands.get(pairMap[i][j]));
+            }      
         }
     }
 
@@ -104,6 +84,8 @@ public class Island {
         this.map = map;
     }
     
+    // create a grid of pairs which builds only one instance for
+    // each cell - used for tracking more easily the neighbors 
     Pair[][] initializePair() {
         Pair[][] pairMap = new Pair[rows][columns];
         for(int i = 0; i < rows; i++)
@@ -130,16 +112,45 @@ public class Island {
     public static void main(String[] args) {
     }
 
-  boolean isLand(int i, int j, HashMap<Pair, Integer> mapIslands, 
-       int rows, int columns, boolean[][] map, int countIslands, 
-       Pair[][] pairMap) {
-      
-        if(i >= rows || i < 0)
-            return false;
-        if(j >= columns || j < 0)
-            return false;
-        if(!map[i][j])
-            return false;
-        return true;
+    boolean isLand(int i, int j, HashMap<Pair, Integer> mapIslands, 
+          int rows, int columns, boolean[][] map, int countIslands, 
+          Pair[][] pairMap) {
+
+           if(i >= rows || i < 0)
+               return false;
+           if(j >= columns || j < 0)
+               return false;
+           if(!map[i][j])
+               return false;
+           return true;
+    }
+
+    private void unifyNeighbors(HashMap<Pair, Integer> mapIslands, int i, int j, Pair[][] pairMap) {
+        int min = 1000;
+        // check neighbours
+        int[] deltaX = {i - 1, i, i, i + 1};
+        int[] deltaY = {j, j - 1, j + 1, j};
+        
+        for(int k = 0; k < 4; k++) {
+            if(isLand(deltaX[k], deltaY[k], mapIslands, rows, columns, map, countIslands, pairMap)){
+                // already mapped neighbor
+                if(mapIslands.containsKey(pairMap[deltaX[k]][deltaY[k]])) {
+                    int neighbor = mapIslands.get(pairMap[deltaX[k]][deltaY[k]]);
+                    if(neighbor < min)
+                        min = neighbor;
+                }
+            }
+        }
+        
+        // unify 2 areas
+        if(min < mapIslands.get(pairMap[i][j])) {
+            countIslands--;
+            mapIslands.put(pairMap[i][j], min);
+            for(int k = 0; k < 4; k++) {
+                if(isLand(deltaX[k], deltaY[k], mapIslands, rows, columns, map, countIslands, pairMap)) {
+                    mapIslands.put(pairMap[deltaX[k]][deltaY[k]], min);
+                }   
+            }
+        }
     }
 }
